7cc3b4d69eba9fdee99e1b95a6cbad4219c0b060
Trevor Hunnes <thunnes@hotmail.com>
2025-10-29 17:43:39 -0700

    Batman

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextType
+from textnode import TextNode
+
+
+def main():
+    text = TextNode("Hello!", TextType.PLAIN_TEXT)
+    print(text)
+
+
+main()
---
+++ src/textnode.py
@@ -0,0 +1,28 @@
+from enum import Enum
+
+
+class TextType(Enum):
+    PLAIN_TEXT = "plain"
+    BOLD_TEXT = "bold"
+    ITALIC_TEXT = "italic"
+    CODE_TEXT = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+
+class TextNode():
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        if self.text == other.text and self.text_type == other.text_type and self.url == other.url:
+            return True
+        return False
+
+    def __repr__(self):
+        if self.url:
+            return f"TextNode(" + self.text + ", " + self.text_type.value + ", " + self.url + ")"
+        else:
+            return f"TextNode(" + self.text + ", " + self.text_type.value + ", None)"
