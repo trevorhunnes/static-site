d9acbfe5f0c180a58a455ff5cb492dad0e4a4206
Trevor Hunnes <thunnes@hotmail.com>
2025-10-31 13:19:34 -0700

    added LeafNode and tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -48,6 +48,24 @@ class TestHTMLNode(unittest.TestCase):
         self.assertEqual(
             node.__repr__(),
             "HTMLNode(i, abcd, children: None, {'href': 'https://gmail.com'})",
+        )
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_no_value(self):
+        node = LeafNode("p", None)
+        with self.assertRaises(ValueError) as assert_error:
+            node.to_html()
+        self.assertEqual(assert_error.exception.args[0], "node has no value")
+
+    def test_children(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+
+        self.assertEqual(
+            node.to_html(),
+            '<a href="https://www.google.com">Click me!</a>',
         )
 
 
