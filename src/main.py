b765c903451776d3f58767ccb974df72863ae347
Trevor Hunnes <thunnes@hotmail.com>
2025-11-06 18:56:38 -0800

    deploying site

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import sys
 from pathlib import Path
 import os
 import shutil
@@ -13,31 +14,36 @@ from markdown import markdown_to_html_node, extract_title
 
 
 def main():
+    if len(sys.argv) == 1:
+        basepath = "/"
+    else:
+        basepath = sys.argv[1]
     generate_public()
-    generate_pages_recursive("./content/", "./template.html", "./public/")
+    generate_pages_recursive(
+        "./content/", "./template.html", "./docs/", basepath)
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     content_path = Path(dir_path_content)
     template = Path(template_path)
     dest_path = Path(dest_dir_path)
-    walk(content_path, content_path, dest_path, template)
+    walk(content_path, content_path, dest_path, template, basepath)
 
 
-def walk(current_dir, content_root, dest_root, template):
+def walk(current_dir, content_root, dest_root, template, basepath):
     for entry in current_dir.iterdir():
         if entry.is_dir():
-            walk(entry, content_root, dest_root, template)
+            walk(entry, content_root, dest_root, template, basepath)
         elif entry.is_file() and entry.suffix == ".md":
             relative = entry.relative_to(content_root)
             out_relative = (relative.with_name(
                 "index.html") if relative.name == "index.md" else relative.with_suffix(".html"))
             out_path = dest_root / out_relative
             out_path.parent.mkdir(parents=True, exist_ok=True)
-            generate_page(str(entry), str(template), str(out_path))
+            generate_page(str(entry), str(template), str(out_path), basepath)
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {
           dest_path} using {template_path}...")
     with open(from_path) as f:
@@ -51,6 +57,8 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(md)
     temp = temp.replace("{{ Title }}", title)
     temp = temp.replace("{{ Content }}", html)
+    temp = temp.replace('href="/', f'href="{basepath}')
+    temp = temp.replace('src="/', f'src="{basepath}')
     with open(dest_path, "w") as f:
         f.write(temp)
     f.close()
@@ -58,9 +66,9 @@ def generate_page(from_path, template_path, dest_path):
 
 def generate_public():
     print("Clearing public directory...")
-    clear_dir("./public/")
+    clear_dir("./docs/")
     print("Copying static files to public directory...")
-    copy_dir("./static/", "./public/")
+    copy_dir("./static/", "./docs/")
 
 
 def copy_dir(src, dst):
